f4ff5953932de15bd5ec4028b9d5bf3cefb48a80
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-08 19:46:11 +0530

    added new

--- components/layout/Footer/Footer.jsx
+++ components/layout/Footer/Footer.jsx
@@ -7,18 +7,19 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="bg-dark py-4">
-      <div className="container-fluid main_container">
+    <div className={styles.footer}>
+      <div className="container-fluid">
         <div className="row align-items-center">
-          <div className="col-lg-8 col-md-6 col-12">
+          <div className="col-xxl-8 col-xl-6 col-lg-6 col-md-6 col-12">
             <Link href="/">
               <Image src={footerLogo} alt="footerLogo" loading="lazy" />
             </Link>
+            <p className="All">© {new Date().getFullYear()} Supaw. All rights reserved.</p>
           </div>
-          <div className="col-lg-4 col-md-6 col-12">
+          <div className="col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-12">
             <div className="row mt-4">
               <div className="col-lg-4 col-md-6 col-12 text-white">
-                <h4 className="fw-bold fs-5 fs-lg-3">About Supaw</h4>
+                <h4>About Supaw</h4>
                 <ul className="list-unstyled mt-4">
                   <li className="mt-3 fw-semibold fs-6">
                     <Link href="/">Who we are</Link>
@@ -38,7 +39,7 @@ export const Footer = () => {
                 </ul>
               </div>
               <div className="col-lg-4 col-md-6 col-12 text-white">
-                <h4 className="fw-bold fs-5 fs-lg-3">Company</h4>
+                <h4>Company</h4>
                 <ul className="list-unstyled mt-4">
                   <li className="mt-3 fw-semibold fs-6">
                     <Link href="/">Link 1</Link>
@@ -55,19 +56,19 @@ export const Footer = () => {
                 </ul>
               </div>
               <div className="col-lg-4 col-md-6 col-12 text-white">
-                <h4 className="fw-bold fs-5 fs-lg-3">Social</h4>
+                <h4>Social</h4>
                 <div className="d-flex gap-3 mt-4 mt-lg-0">
-                  <div className=" fs-3 text-center  rounded-5">
-                    <Link href="/">
+                  <div className={styles.social}>
+                    <Link href="/" >
                       <i className="bi bi-facebook"></i>
                     </Link>
                   </div>
-                  <div className="fs-3  text-center  rounded-5">
+                  <div className={styles.social}>
                     <Link href="/">
                       <i className="bi bi-twitter"></i>
                     </Link>
                   </div>
-                  <div className=" fs-3 text-center  rounded-5">
+                  <div className={styles.social}>
                     <Link href="/">
                       <i className="bi bi-linkedin"></i>
                     </Link>
